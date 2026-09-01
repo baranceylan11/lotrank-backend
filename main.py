ef688789f250f6b42fe5ca945b4bfbfa1951b4c5
baranceylan11 <bgceylan@outlook.com>
2026-09-01 04:30:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import psycopg2
 from fastapi import FastAPI
 
 app = FastAPI(title="LotRank Backend")
@@ -9,3 +11,25 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
+@app.get("/db-test")
+def db_test():
+    try:
+        conn = psycopg2.connect(os.environ["DATABASE_URL"])
+        cur = conn.cursor()
+        cur.execute("SELECT 1;")
+        result = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        return {
+            "status": "ok",
+            "database": "connected",
+            "result": result[0]
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "database": "not_connected",
+            "detail": str(e)
+        }
